154ae0e8ea1a834cc8870489585770e0fac4a512
Alisha Shahbazian <alishashahbazian@gmail.com>
2018-05-18 15:39:33 -0700

    first commit

---
+++ client/app.js
@@ -0,0 +1,144 @@
+const socket = io()
+
+// have at least one component 
+const projectsComponent ={
+    template : `<div>
+                    <div v-for="proj in projects">
+                        <div class="border border-info rounded m-4 d-flex flex-row justify-content-around flex-wrap" :class="{ 'bg-secondary': proj.active }" >
+                            <h2 v-on:click="setActive(proj)"  >{{ proj.name }}</h2>
+                            <span class="icon mt-2">
+                                <a @click="removeProj(proj)" class="fa fa-trash text-danger fa-2x" ></a>
+                             </span>
+                             
+                        </div>
+                    </div>
+                </div>`,
+    props: ['projects']
+}
+
+const todosComponent ={
+    template : `<div class="container">
+                    <h1 style="border-style: double" >{{proj.name}}</h1>
+                        <div v-for="todo in proj.todos">
+                            <div class="m-2 pl-3 pr-3 d-flex justify-content-start align-items-center border border-info rounded">
+                                <div class="d-flex flex-column">
+                                    <input type="checkbox" :checked= "todo.done" v-on:change="toogle(todo)">
+                                </div>
+                                <div class="mt-2 ml-3">
+                                 <p v-show="!todo.done">{{todo.description}}</p>
+                                 <p v-show="todo.done"><s>{{todo.description}}</s><p>
+                                </div>
+                                <div class="ml-auto pl-2">
+                                    <span class="icon">
+                                        <a @click="removeTodo(todo)" class="fa fa-trash text-danger fa-lg" ></a>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                </div>`,
+    props: ['proj' ]
+}
+
+const app = new Vue({
+    el: '#todo-app',
+    data:{
+        selected: false,
+        project: '',
+        error: '',
+        todo:'',
+        projects: [],
+        currentProj : {},
+    },
+    methods :{
+        // Angie
+        addProject: function(){
+            socket.emit('addProject', this.project)
+
+        }, 
+        addTodo: function(){
+            socket.emit('addTodo', {p_id : this.currentProj._id, description : this.todo})
+            this.todo =''
+        },
+        clearCompleted: function(){
+            socket.emit('clearCompleted', this.currentProj)
+        },
+        archive: function(){
+            socket.emit('archive', this.currentProj)
+        }
+    },
+
+    components:{
+        'todos-component' : todosComponent,
+        'projects-component' : projectsComponent
+    }
+
+})
+
+//daniel
+const setActive = proj =>{
+    socket.emit('setActive', proj)
+    socket.emit('getTodos', proj)
+    app.selected = true
+    app.currentProj = proj
+    app.error=''
+}
+
+//william
+const removeProj = proj => {
+    app.selected= false
+    socket.emit('removeProj', proj)
+}
+
+//alisha
+const removeTodo = (todo) => {
+    socket.emit('removeTodo', todo)
+}
+
+//jurelly
+const toogle = (todo) =>{
+    socket.emit('updateTodos', todo)
+}
+
+
+socket.on('refresh-projects', projects =>{
+    app.projects = projects
+})
+
+// Angie
+socket.on('successful-project', project =>{
+    app.error=''
+    app.projects.push(project)
+    app.project=''
+})
+
+socket.on('unsuccessful-project', e =>{
+    app.error = `Project '${app.project}' already exists`
+    app.project=''
+})
+
+//daniel
+socket.on('activeProj', projects =>{
+    app.projects = projects
+ })
+ 
+ socket.on('set-active', project =>{
+     if(project){
+         setActive(project)
+     }
+     else{
+         app.selected = false
+         app.currentProj={}
+     }
+ })
+
+socket.on('successful-todo', todo => {
+    app.currentProj.todos.push(todo)
+})
+//william
+socket.on('successful-removeProj', proj => {
+    app.projects.splice(app.projects.findIndex(item => item._id == proj._id), 1)
+})
+//jurelly & alisha
+socket.on('get-todos', todos =>{
+    app.currentProj.todos = todos
+})
---
+++ server/sockets.js
@@ -0,0 +1,72 @@
+module.exports = (server, db) => {
+    const
+        io = require('socket.io')(server),
+        moment = require('moment')
+
+    io.on('connection', socket => {
+        //socket events
+
+        db.allProjects()
+            .then(projects => socket.emit('refresh-projects', projects))
+
+        //daniel
+        db.findActive()
+            .then(project => socket.emit('set-active', project))
+        // Angie
+        socket.on("addProject", project => {
+            db.createProject(project)
+                .then(created => io.emit('successful-project', created))
+                .catch(e => io.emit('unsuccessful-project', e))
+        }),
+            socket.on('addTodo', todo => {
+                db.createTodo(todo)
+                    .then(created => io.emit('successful-todo', created))
+            }),
+            //daniel
+            socket.on('setActive', proj => {
+                db.activeProj(proj)
+                    .then(project => io.emit('activeProj', project))
+            }),
+            //wiliam
+            socket.on('removeProj', proj => {
+                db.removeProj(proj)
+                    .then(created => {
+                        io.emit('successful-removeProj', proj)
+                    })
+            }),
+            socket.on('archive', currentProj => {
+                db.archiveTodos()
+                    .then(removeTodo => {
+                        if (currentProj)
+                            db.getTodos(currentProj._id)
+                                .then(todo => io.emit('get-todos', todo))
+                    })
+            }),
+            //jurely
+            socket.on('getTodos', proj => {
+                db.getTodos(proj._id)
+                    .then(todos => io.emit('get-todos', todos))
+            }),
+            socket.on('updateTodos', todo => {
+                db.updateTodos(todo)
+                    .then(todos => io.emit('get-todos', todos))
+            }),
+            //alisha
+            socket.on('removeTodo', todo => {
+                db.removeTodo(todo)
+                    .then(remove => {
+                        db.getTodos(todo.p_id)
+                            .then(todos => {
+                                io.emit('get-todos', todos)
+                            })
+                    })
+            }),
+            socket.on('clearCompleted', currentProj => {
+                db.clearTodo(currentProj)
+                    .then(removed => {
+                        db.getTodos(currentProj._id)
+                            .then(todo => io.emit('get-todos', todo))
+                    })
+            })
+    })
+}
